346a90f564eac861dd5009c1b7849d3553483e87
Matthew Hounslow <hounslowmatthew@gmail.com>
2025-11-10 22:30:31 -0800

    stage index.html

--- test/controllers/essays_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EssaysControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get essays_index_url
-    assert_response :success
-  end
-
-end
--- test/controllers/resume_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ResumeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get resume_index_url
-    assert_response :success
-  end
-
-end
